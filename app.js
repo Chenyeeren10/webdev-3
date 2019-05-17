23dd0391ee75da4c72e17a9f4f73fa4dd0a813d7
Chenyeeren10 <48347987+Chenyeeren10@users.noreply.github.com>
2019-05-17 17:58:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ alert("Hello world");
 var target = document.getElementById("target");
 target.innertext = "Hi";
 var apple = document.getElementById("apple");
-button.addEventListener('click', function() {
-  alert("yes thanks for clicking me");
-});
+button.addEventListener('click', function() 
+  var username = document.getElementById("username");
+  alert(username.value)
+  if (username.value =="admin") {
+    alert("welcome")
+  } else {
+    alert("youre not admin")
+  }
+
+}
+  console.log("username")
