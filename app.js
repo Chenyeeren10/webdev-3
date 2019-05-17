4693367c2608b6d6d56a165f41f1769ba5fb16de
Chenyeeren10 <48347987+Chenyeeren10@users.noreply.github.com>
2019-05-17 17:50:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 console.log("Hello world");
 alert("Hello world");
+var target = document.getElementById("target");
+target.innertext = "Hi";
+var apple = document.getElementById("apple");
+button.addEventListener('click', function() {
+  alert("yes thanks for clicking me");
+});
